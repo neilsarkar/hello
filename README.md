48bfd81c52936af06d87603d20dae14bf425ba72
Neil Sarkar <neil.r.sarkar@gmail.com>
2015-02-15 12:24:26 -0800

    Minify in production

---
+++ README.md
@@ -0,0 +1,12 @@
+Admin App
+==
+
+You love it.
+
+Sawdust:
+
+* fingerprinted assets in prod
+* reload properly when manifest changes
+* use gulp-load-plugins
+* sourcemaps
+* wiredeps
---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    fs = require('fs'),
+    uglify = require('gulp-uglify'),
+    paths;
+
+// read paths on load
+readPaths()
+function readPaths() {
+  return paths = JSON.parse(fs.readFileSync('./manifest.json', 'utf8'))
+}
+
+var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+gulp.task('js', function() {
+  var stream = gulp.src(paths.js).
+    pipe(concat('application.js'));
+
+  if( !isDevelopment ) {
+    stream = stream.pipe(uglify())
+  }
+
+  return stream.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('css', function() {
+  var stream = gulp.src(paths.css).
+    pipe(sass({outputStyle: isDevelopment ? 'nested' : 'compressed'})).
+    on('error', function(err) { console.error("SCSS compile error:" + err.message); this.emit('end'); }).
+    pipe(concat('application.css')).
+    pipe(gulp.dest('dist/'));
+
+  return isDevelopment ?
+    stream.pipe(browserSync.reload({stream: true})) :
+    stream;
+})
+
+gulp.task('img', function() {
+  return gulp.src(paths.img, {base: 'app/img/'}).
+    pipe(gulp.dest('dist/images/'));
+})
+
+gulp.task('html', function() {
+  return gulp.src(paths.html, {base: 'app/'}).
+    pipe(gulp.dest('dist'));
+})
+
+gulp.task('build', ['js', 'img', 'css', 'html'])
+
+if( !process.env.NODE_ENV || process.env.NODE_ENV == 'development' ) {
+  var server = require('gulp-express'),
+      browserSync = require('browser-sync');
+
+  // Watches asset paths and reloads browser on changes
+  gulp.task('watch', function() {
+    setWatchers()
+
+    // Restart process when gulpfile is changed
+    gulp.watch('gulpfile.js', function() {
+      console.log("Gulpfile changed, you should restart")
+      process.exit(0)
+    })
+
+    // Reset paths and watchers when manifest.json is changed
+    gulp.watch('manifest.json', function() {
+      readPaths()
+      setWatchers()
+      gulp.run('build')
+    })
+
+    process.stdin.on('data', function(line) {
+      if( line.toString() === "\n" ) {
+        gulp.run('build')
+      }
+    })
+
+    // (Re)sets watchers
+    function setWatchers() {
+      gulp.watch(paths.js, ['js', browserSync.reload])
+      gulp.watch(paths.css, ['css']) // browserSync automatically reloads
+      gulp.watch(paths.html, ['html', browserSync.reload])
+      gulp.watch(paths.img, ['img', browserSync.reload])
+    }
+  })
+
+  gulp.task('server', function() {
+    server.run({
+      file: 'index.js'
+    })
+
+    browserSync({
+      proxy: 'localhost:3000',
+      port: 5000
+    })
+
+    gulp.watch(['index.js'], [server.run])
+  })
+  gulp.task('default', ['build', 'server', 'watch'])
+}
